a34a43246d33189fab6d1b1cab7d2e376493ea51
o0anais0o <choisy_anais@hotmail.fr>
2025-07-10 14:55:11 +0200

    Modification

--- hbnb/part3/app/__init__.py
+++ hbnb/part3/app/__init__.py
@@ -26,10 +26,12 @@ def create_app(config_name='default'):
     from app.api.v1.places import api as places_ns
     from app.api.v1.amenities import api as amenities_ns
     from app.api.v1.reviews import api as reviews_ns
+    from app.api.v1.auth import api as auth_ns
 
     api.add_namespace(users_ns, path='/api/v1/users')
     api.add_namespace(places_ns, path='/api/v1/places')
     api.add_namespace(amenities_ns, path='/api/v1/amenities')
     api.add_namespace(reviews_ns, path='/api/v1/reviews')
+    api.add_namespace(auth_ns, path='/api/v1/auth')
 
     return app
--- hbnb/part3/app/api/v1/auth.py
+++ hbnb/part3/app/api/v1/auth.py
@@ -1,5 +1,7 @@
 from flask_restx import Namespace, Resource, fields
 from app.services.auth import login_user  # ← appel au service
+from flask_jwt_extended import create_access_token
+from app.services.facade import HBnBFacade
 
 api = Namespace('auth', description='Auth operations')
 
@@ -19,10 +21,16 @@ error_model = api.model('Error', {
 @api.route('/login')
 class Login(Resource):
     @api.expect(login_model, validate=True)
+    @api.marshal_with(token_model)
     @api.response(200, 'Success', token_model)
     @api.response(401, 'Invalid credentials', error_model)
     def post(self):
         data = api.payload
+        data = api.payload
+        user = HBnBFacade().get_user_by_email(data['email'])
+        if not user or not user.check_password(data['password']):
+            api.abort(401, "Invalid credentials")
+        token = create_access_token(identity=str(user.id))
         email = data.get('email')
         password = data.get('password')
         if not email or not password:
